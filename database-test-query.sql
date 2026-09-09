e9e306ff25154a1a33273597ff38b83695dee75b
Ikbal Yuliyanto <ikbalyuliyanto7@gmail.com>
2026-09-09 20:36:45 +0700

    wip: soal 4 pseudocode + soal 3 queries (2,3,4)

--- database-test-query.sql
+++ database-test-query.sql
@@ -1,52 +1,57 @@
--- ============================================================
--- SOAL 3 - SQL : SIPERPUS (Sistem Informasi Perpustakaan)
--- Kandidat: Ikbal Yuliyanto
--- Dialect target: PostgreSQL (sesuaikan bila pakai MySQL)
--- ============================================================
+-- SOAL 1
+categories
+    |
+    | 1
+    |
+    | N
+  books
+    |
+    | 1
+    |
+    | N
+  loans
+    |
+    | N
+    |
+    | 1
+  users
+
+-- SOAL 2
+
+SELECT b.judul AS Buku
+FROM books b
+LEFT JOIN loans l ON b.id_book = l.id_book
+WHERE l.id_book IS NULL;
 
 
--- ------------------------------------------------------------
--- 1. SCHEMA  (DDL)
---    Buat tabel: categories, users, books, borrowings
---    (tambahkan tabel/kolom lain bila perlu)
--- ------------------------------------------------------------
+-- SOAL 3
 
--- TODO: CREATE TABLE categories (...)
--- TODO: CREATE TABLE users (...)         -- nama, alamat, no_ktp, no_hp, email
--- TODO: CREATE TABLE books (...)         -- judul, pengarang, penerbit, isbn, tahun_terbit, stok, category_id
--- TODO: CREATE TABLE borrowings (...)    -- user_id, book_id, borrow_date, due_date, return_date
-
-
--- ------------------------------------------------------------
--- 2. INITIAL DATA (DML)
---    5 kategori, 5 user, 10 buku, 9 peminjaman
---    User1 -> buku 1-3, User2 -> buku 4-6, User3 -> buku 7-9
---    User3 telat kembalikan 1 buku selama 5 hari
--- ------------------------------------------------------------
-
--- TODO: INSERT INTO categories ...
--- TODO: INSERT INTO users ...
--- TODO: INSERT INTO books ...
--- TODO: INSERT INTO borrowings ...
+SELECT
+    u.nama AS User,
+    CONCAT(
+        'Rp',
+        SUM(
+            DATEDIFF(l.tanggal_kembali, l.tanggal_jatuh_tempo) * 1000
+        )
+    ) AS Denda
+FROM users u
+JOIN loans l ON u.id_user = l.id_user
+WHERE l.tanggal_kembali > l.tanggal_jatuh_tempo
+GROUP BY u.id_user, u.nama;
 
 
--- ------------------------------------------------------------
--- 3. QUERY JAWABAN
--- ------------------------------------------------------------
+-- SOAL 4
 
--- 3a. Daftar buku yang TIDAK PERNAH dipinjam siapapun
---     expected: Buku 10
--- TODO
-
-
--- 3b. User yang pernah telat mengembalikan buku + dendanya (Rp1.000/hari)
---     expected: User 3 | Rp5000
--- TODO
-
-
--- 3c. Tiap user beserta daftar buku yang dipinjamnya (digabung 1 kolom)
---     expected:
---       1 | User 1 | Buku 3, Buku 2, Buku 1
---       2 | User 2 | Buku 6, Buku 5, Buku 4
---       3 | User 3 | Buku 9, Buku 8, Buku 7
--- TODO
+SELECT
+    ROW_NUMBER() OVER (ORDER BY u.id_user) AS No,
+    u.nama AS User,
+    GROUP_CONCAT(
+        b.judul
+        ORDER BY b.id_book DESC
+        SEPARATOR ', '
+    ) AS Buku
+FROM users u
+JOIN loans l ON u.id_user = l.id_user
+JOIN books b ON l.id_book = b.id_book
+GROUP BY u.id_user, u.nama
+ORDER BY u.id_user;
